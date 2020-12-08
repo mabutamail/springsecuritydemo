fdc221c14bad32dbe056bae12a9b47f251f18d56
mabuta <mabutamail@gmail.com>
2020-12-08 18:29:14 +0300

    init

---
+++ src/main/java/com/github/mabutamail/springsecuritydemo/model/Developer.java
@@ -0,0 +1,12 @@
+package com.github.mabutamail.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
